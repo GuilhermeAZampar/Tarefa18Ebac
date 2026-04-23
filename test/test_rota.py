6e4a9ebcc1e6d29ddf3a3cb2930ed2cfde0735cc
Guilherme Zampar <guilhermezampar90@gmail.com>
2026-04-23 11:15:44 -0300

    adicionando tarefa

---
+++ test/test_rota.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from main import app
+import pytest
+
+client=TestClient(app)
+
+def test_rota_get():
+    response=client.get("/")
+    assert response.status_code==200
+    assert response.json()=={"message":"Hello Word"}
